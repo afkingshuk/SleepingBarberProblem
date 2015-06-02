30eb824a0b51de84fde568f57cad27f8f09a899a
Tomas Valek <tomasvalek@post.cz>
2015-06-02 14:45:30 +0200

    Comments and code style.

--- barbers.c
+++ barbers.c
@@ -3,8 +3,17 @@
  * Date:   	04.2011
  * Author:  Tomas Valek, xvalek02@stud.fit.vutbr.cz
  * Project: IOS project number 2 - Synchronization of proceses
- * Desc.:   Implement of modified variant of barber sleeping problem using semaphores.
- */
+ * Desc.:   Implement of modified variant of barber sleeping problem using semaphores
+ *			and shared memory. Prevent to deadlock and starvation.
+ * Params:  
+ *	./barbers Q genc genb N F
+ *	Q - number of chairs in waiting room
+ *	genc - interval of generate customers [ms]
+ *	genb - interval of generate of time service [ms]
+ *	N - number of customers
+ *	F - output filename
+*/
+
 #include <stdio.h>		//standard input/output library functions
 #include <stdlib.h>		//general funciton of language C
 #include <limits.h>		//UINT_MAX
@@ -20,8 +29,7 @@
 #include <time.h>		//srand
 #include <signal.h>		//signals
 
-
-//Definuji si jmena pro semafory:
+//Definition name of semaphores:
 #define SEM_NAME_CEKARNA "/xvalek02_cekarna"
 #define SEM_NAME_VYPIS "/xvalek02_vypis"
 #define SEM_NAME_HOLIC_SPI "/xvalek02_holic_spi"
@@ -29,47 +37,40 @@
 #define SEM_NAME_HOLIC_DOSTRIHAL "/xvalek02_holic_dostrihal"
 #define SEM_NAME_HOLIC_POKRACUJ "/xvalek02_ready_vypisy"
 
+//TODO magicka cisla
 //TODO codestyle upravit
 //TODO dopsat README
 //TODO promenne prepsat do english
-//TODO upravit makefile
 
-typedef struct
-	{//struktura pro parametry programu
-		unsigned int pocet_zidli;			//pocet zidli v cekarne
-		unsigned int genc;					//interval generovani zakazniku		TODO jaky interval?
-		unsigned int genb;					//interval obsluhy zakaznika
-		unsigned int pocet_zakazniku;		//pocet zakazniku, kteri maji prijit
-		char *soubor;						//soubor
-	}P_parametry;
+typedef struct {
+//Params of application
+		unsigned int numberOfChairs;		//number of chairs in waiting room
+		unsigned int genc;					//interval of generate customers [ms]
+		unsigned int genb;					//interval of generate of time service [ms]
+		unsigned int numberOfCustomers;		//number of customers
+		char *file;							//output file
+} P_param;
 
-typedef struct
-	{//sdilena struktura
-		unsigned int aktualni_zakaznik;		//aktualne zpracovavany zakaznik
-		unsigned int poradi_operace;		//poradi aktualni provadene operace
-		unsigned int volne_zidle;			//pocet volnych zidli
-	}S_sdilena_data;
+typedef struct {
+//shared struct
+		unsigned int currentCustomer;		//current processed customer
+		unsigned int orderOfOperations;		//order of operation
+		unsigned int numberOfFreeChairs;	//number of free chairs for new customers
+} SSharedData;
 
-//globalni promene:
+//global variables:
 sem_t *xvalek02_cekarna;
-
 sem_t *xvalek02_vypis;
-
 sem_t *xvalek02_holic_spi;
-
 sem_t *xvalek02_holicova_zidle;
-
 sem_t *xvalek02_holic_dostrihal;
-
 sem_t *xvalek02_ready_vypisy;
-
-FILE *vyst_soubor;
-
-S_sdilena_data * sdilena_data;
+FILE *outputFile;
+SSharedData * sharedData;
 
 
-void znic_semafory(sem_t *xvalek02_cekarna, sem_t *xvalek02_vypis, sem_t *xvalek02_holic_spi, sem_t *xvalek02_holicova_zidle, sem_t *xvalek02_holic_dostrihal, sem_t *xvalek02_ready_vypisy )
-	{ //funkce odstrani vsechny vytvorene semafory ze systemu
+//Release all semaphores.
+void cleanSemaphores(sem_t *xvalek02_cekarna, sem_t *xvalek02_vypis, sem_t *xvalek02_holic_spi, sem_t *xvalek02_holicova_zidle, sem_t *xvalek02_holic_dostrihal, sem_t *xvalek02_ready_vypisy ) {
 		sem_close(xvalek02_cekarna);
 		sem_unlink(SEM_NAME_CEKARNA);
 
@@ -87,82 +88,75 @@ void znic_semafory(sem_t *xvalek02_cekarna, sem_t *xvalek02_vypis, sem_t *xvalek
 
 		sem_close(xvalek02_ready_vypisy);
 		sem_unlink(SEM_NAME_HOLIC_POKRACUJ);
-	}
-
-int konvertuj (char *parametr)
-{//funkce prekonvertuje parametr na cislo
-
-	int cislo = atoi(parametr);
-	if ( cislo > 0 ) 	return cislo;
-	else if ( cislo == 0 )
-	{//musime jeste zkontrolovat zda byla zadana 0 nebo chybny parametr
-		int kontrola = 0;
-		kontrola = strcmp("0", parametr);
-		if ( kontrola == cislo )	return cislo;
-		else if ( kontrola < 0 )	return UINT_MAX;
-	}
-return UINT_MAX;
 }
 
-int over_parametry(int argc, char *argv[], P_parametry *p)
-	{//overime spravnost parametru
-		if (argc != 6) return 1;
+//Char's parameter to number parameter.
+int convert(char *param) {
 
-		p->pocet_zidli = konvertuj(argv[1]);
-		p->genc = konvertuj(argv[2]);
-		p->genb = konvertuj(argv[3]);
-		p->pocet_zakazniku = konvertuj(argv[4]);
-		p->soubor = argv[5];
+	int number = atoi(param);
+	if ( number > 0 )
+		return number;
+	else if ( number == 0 ) {
+		int check = strcmp("0", param);
+		if ( check == number )
+			return number;
+		else if ( check < 0 )
+			return UINT_MAX;
+	}
 
-		if (p->pocet_zidli == UINT_MAX || p->genc == UINT_MAX || p->genb == UINT_MAX || p->pocet_zakazniku == UINT_MAX || strlen(p->soubor) == UINT_MAX)
-		{//strlen nam vrati pocet znaku v stringu
-		 //UINT_MAX v nasem pripade indikuje chybu z konverze
+	return UINT_MAX;
+}
+
+//Verify parameters.
+int verifyParameters(int argc, char *argv[], P_param *p) {
+		if (argc != 6)
 			return 1;
-		} 
-		else return 0;
-	}
 
-unsigned int sdilena_pamet(int pam, int n)
-	{
-		unsigned int hodnota;
-		unsigned int *pamet;
-		pamet = shmat(pam, NULL, 0);
-		hodnota = (*pamet);
-		if (n == -1) *pamet = 1;
-		else if (n == 1) (*pamet)++;
-		shmdt(pamet);
-		return hodnota;
-	}
+		p->numberOfChairs = convert(argv[1]);
+		p->genc = convert(argv[2]);
+		p->genb = convert(argv[3]);
+		p->numberOfCustomers = convert(argv[4]);
+		p->file = argv[5];
 
-void ukonceni_holice(int signum)
-{//uklid
-	if (vyst_soubor != stdout) fclose(vyst_soubor);
+		if (p->numberOfChairs == UINT_MAX || p->genc == UINT_MAX || p->genb == UINT_MAX || p->numberOfCustomers == UINT_MAX || strlen(p->file) == UINT_MAX)
+			return 1;
+
+	return 0;
+}
+
+//Clean.
+void clean(int signum) {
+	if (outputFile != stdout)
+		fclose(outputFile);
+
 	sem_close(xvalek02_cekarna);
 	sem_close(xvalek02_vypis);
 	sem_close(xvalek02_holic_spi);
 	sem_close(xvalek02_holicova_zidle);
 	sem_close(xvalek02_holic_dostrihal);
 	sem_close(xvalek02_ready_vypisy);
-	shmdt((void*)sdilena_data);
+
+	shmdt((void*)sharedData);
 	fflush(stdout);
 	exit(signum);
 }
 
-/****************HLAVNI PROGRAM****************/
-int main(int argc, char *argv[])
-{
-	signal(SIGUSR1, ukonceni_holice);
+/****************MAIN****************/
+int main(int argc, char *argv[]){
 
-	srand(time(0));//kvuli rand()
-	P_parametry p;
-	if ( over_parametry(argc, argv, &p) == 1 )
-	{//overime spravne zadane parametry
-		fprintf(stderr,"Spatne zadane argumenty.\n");
+	signal(SIGUSR1, clean);
+
+	srand(time(0));
+	P_param p;
+
+	//verify correct parameters
+	if ( verifyParameters(argc, argv, &p) == 1 ) {
+		fprintf(stderr,"Bad parameters.\n");
 		return 1;
 	}
 
-	//vytvorime vsechny potrebne semafory:
-	xvalek02_cekarna = sem_open(SEM_NAME_CEKARNA, O_CREAT | O_EXCL, 0600, p.pocet_zidli);
+	//create semaphores:
+	xvalek02_cekarna = sem_open(SEM_NAME_CEKARNA, O_CREAT | O_EXCL, 0600, p.numberOfChairs);
 
 	xvalek02_vypis = sem_open(SEM_NAME_VYPIS, O_CREAT | O_EXCL, 0600, 1);
 
@@ -174,206 +168,212 @@ int main(int argc, char *argv[])
 
 	xvalek02_ready_vypisy = sem_open(SEM_NAME_HOLIC_POKRACUJ, O_CREAT | O_EXCL, 0600, 0);
 
-	if ( xvalek02_cekarna == SEM_FAILED || xvalek02_vypis == SEM_FAILED || xvalek02_holic_spi == SEM_FAILED || xvalek02_holicova_zidle == SEM_FAILED  || xvalek02_holic_dostrihal == SEM_FAILED || xvalek02_ready_vypisy == SEM_FAILED )
-	{//pokud se nepodari vytvorit jeden ze semaforu
-		znic_semafory(xvalek02_cekarna, xvalek02_vypis, xvalek02_holic_spi, xvalek02_holicova_zidle, xvalek02_holic_dostrihal, xvalek02_ready_vypisy );
-		perror("sem_open() chyba");
+	//create semaphores failed
+	if ( xvalek02_cekarna == SEM_FAILED || xvalek02_vypis == SEM_FAILED || xvalek02_holic_spi == SEM_FAILED || xvalek02_holicova_zidle == SEM_FAILED  || xvalek02_holic_dostrihal == SEM_FAILED || xvalek02_ready_vypisy == SEM_FAILED ){
+		cleanSemaphores(xvalek02_cekarna, xvalek02_vypis, xvalek02_holic_spi, xvalek02_holicova_zidle, xvalek02_holic_dostrihal, xvalek02_ready_vypisy );
+		perror("sem_open() fail.");
 		return 1;
 	}
 
-	unsigned int i = 1; //pouzito v cyklu pro vytvareni poctu zakazniku
-	int pamet1; // jmena sdilenych pameti
+	unsigned int i = 1; 	//used in cycle for create of number of customer
+	int sharedMemory;
 
-	//vystupni soubor:
-	if (strcmp(p.soubor,"-") == 0) vyst_soubor = stdout;
-	else
-	{
-		vyst_soubor = fopen(p.soubor, "w");
-		if (vyst_soubor == NULL)
-		{//kontrola zda se vytvoril soubor
-			znic_semafory(xvalek02_cekarna, xvalek02_vypis, xvalek02_holic_spi, xvalek02_holicova_zidle, xvalek02_holic_dostrihal, xvalek02_ready_vypisy);
-			perror("fopen() chyba");
+	//output file:
+	if (strcmp(p.file,"-") == 0)
+		outputFile = stdout;
+	else {
+		outputFile = fopen(p.file, "w");
+
+		//check is file created correct
+		if (outputFile == NULL)	{
+			cleanSemaphores(xvalek02_cekarna, xvalek02_vypis, xvalek02_holic_spi, xvalek02_holicova_zidle, xvalek02_holic_dostrihal, xvalek02_ready_vypisy);
+			perror("fopen() fail.");
 			return 1;
 		}
 	}
 
-	//generujeme IPC klice:
-	key_t klic1 = ftok(argv[0],0);
-	if ( klic1 == -1 )
-	{//jeden z klicu se nevytvoril
-		if (vyst_soubor != stdout) fclose(vyst_soubor);
-		znic_semafory(xvalek02_cekarna, xvalek02_vypis, xvalek02_holic_spi, xvalek02_holicova_zidle, xvalek02_holic_dostrihal, xvalek02_ready_vypisy);
-		perror("ftok() chyba");
+	//generating of IPC keys:
+	key_t IPCkey = ftok(argv[0], 0);
+
+	//once of keys did not created
+	if ( IPCkey == -1 ) {
+		if (outputFile != stdout)
+			fclose(outputFile);
+		cleanSemaphores(xvalek02_cekarna, xvalek02_vypis, xvalek02_holic_spi, xvalek02_holicova_zidle, xvalek02_holic_dostrihal, xvalek02_ready_vypisy);
+		perror("ftok() fail.");
 		return 1;
 	}
 
-	//inicializujeme sdilenou pamet:
-	pamet1 = shmget(klic1, sizeof(S_sdilena_data), IPC_CREAT | 0600);
-	if ( pamet1 == -1 )
-	{//chyba
-		if ( pamet1 == -1 ) shmctl(pamet1,IPC_RMID, NULL);
-		if (vyst_soubor != stdout) fclose(vyst_soubor);
-		znic_semafory(xvalek02_cekarna, xvalek02_vypis, xvalek02_holic_spi, xvalek02_holicova_zidle, xvalek02_holic_dostrihal, xvalek02_ready_vypisy);
-		perror("shmget() chyba");
+	//init of shared memory:
+	sharedMemory = shmget(IPCkey, sizeof(SSharedData), IPC_CREAT | 0600);
+	if ( sharedMemory == -1 ) {//error
+		if ( sharedMemory == -1 )
+			shmctl(sharedMemory,IPC_RMID, NULL);
+
+		if (outputFile != stdout)
+			fclose(outputFile);
+
+		cleanSemaphores(xvalek02_cekarna, xvalek02_vypis, xvalek02_holic_spi, xvalek02_holicova_zidle, xvalek02_holic_dostrihal, xvalek02_ready_vypisy);
+		perror("shmget() fail.");
 		return 1;
 	}
 
+	//connect shared memory
+	sharedData = (SSharedData*)shmat(sharedMemory, NULL, 0);
+	if ( sharedData == NULL) {//error
+		if (outputFile != stdout)
+			fclose(outputFile);
 
-	//pripojime sdilenou pamet
-	sdilena_data = (S_sdilena_data*)shmat(pamet1, NULL, 0);
-	if ( sdilena_data == NULL)
-	{//chyba
-		if (vyst_soubor != stdout) fclose(vyst_soubor);
-		znic_semafory(xvalek02_cekarna, xvalek02_vypis, xvalek02_holic_spi, xvalek02_holicova_zidle, xvalek02_holic_dostrihal, xvalek02_ready_vypisy);
-		shmdt((void*)sdilena_data);
-		shmctl(pamet1,IPC_RMID, NULL);
-		perror("shmat() chyba");
+		cleanSemaphores(xvalek02_cekarna, xvalek02_vypis, xvalek02_holic_spi, xvalek02_holicova_zidle, xvalek02_holic_dostrihal, xvalek02_ready_vypisy);
+		shmdt((void*)sharedData);
+		shmctl(sharedMemory,IPC_RMID, NULL);
+		perror("shmat() fail.");
 		return 1;
 	}
 
-	sdilena_data->aktualni_zakaznik = 1;
-	sdilena_data->poradi_operace = 1;
-	sdilena_data->volne_zidle = p.pocet_zidli;
+	sharedData->currentCustomer = 1;
+	sharedData->orderOfOperations = 1;
+	sharedData->numberOfFreeChairs = p.numberOfChairs;
 
 	pid_t id = fork();
 
-	if ( id == -1 )
-	{//neporadilo se vytvorit proces
-		if (vyst_soubor != stdout) fclose(vyst_soubor);
-		shmdt((void*)sdilena_data);
-		shmctl(pamet1,IPC_RMID, NULL);
-		znic_semafory(xvalek02_cekarna, xvalek02_vypis, xvalek02_holic_spi, xvalek02_holicova_zidle, xvalek02_holic_dostrihal, xvalek02_ready_vypisy);
-		perror("fork() chyba");
+	if ( id == -1 )	{ //fork fail
+		if (outputFile != stdout)
+			fclose(outputFile);
+
+		shmdt((void*)sharedData);
+		shmctl(sharedMemory,IPC_RMID, NULL);
+		cleanSemaphores(xvalek02_cekarna, xvalek02_vypis, xvalek02_holic_spi, xvalek02_holicova_zidle, xvalek02_holic_dostrihal, xvalek02_ready_vypisy);
+		perror("fork() fail.");
 		exit(1);
-	}
+	} else if ( id == 0 ) {//process of barber
 
-	else if ( id == 0 )
-	{//proces holice
-
-		while (1)
-		{//cyklus holice
+		while (1) {//cycle of barber
 
 			sem_wait(xvalek02_vypis);
-			fprintf(vyst_soubor, "%u: barber: checks\n", sdilena_data->poradi_operace++);//holic zkontroluje cekarnu
-			fflush(vyst_soubor);
+			fprintf(outputFile, "%u: barber: checks\n", sharedData->orderOfOperations++); 	//holic checks waiting room
+			fflush(outputFile);
 			sem_post(xvalek02_vypis);
 
-			sem_wait(xvalek02_holic_spi);//holic jde spat
+			sem_wait(xvalek02_holic_spi);													//barber goes sleep
 
 			sem_wait(xvalek02_vypis);
-			fprintf(vyst_soubor, "%u: barber: ready\n", sdilena_data->poradi_operace++);//holic je pripraven
-			fflush(vyst_soubor);
+			fprintf(outputFile, "%u: barber: ready\n", sharedData->orderOfOperations++);	//barber is ready
+			fflush(outputFile);
 			sem_post(xvalek02_vypis);
 
-			sem_post(xvalek02_ready_vypisy);//semafor aby byl ready holic pred ready zakaznik
+			sem_post(xvalek02_ready_vypisy);	//semaphore, because "barber ready" must be earlier than "customer ready"
 
-			sem_post(xvalek02_holicova_zidle);//povolime zakaznikovi si sednou do kresla
+			sem_post(xvalek02_holicova_zidle);	//customer can sit to barber's chair
 
-			usleep(rand() % ((p.genb*1000)+1)); //cas obsluhy holice dany parametrem
+			usleep(rand() % ((p.genb*1000)+1)); //barber is cutting customer
 
 			sem_wait(xvalek02_vypis);
-			fprintf(vyst_soubor, "%u: barber: finished\n", sdilena_data->poradi_operace++);//ceka nez holic zkontroluje cekarnu
-			fflush(vyst_soubor);
+			fprintf(outputFile, "%u: barber: finished\n", sharedData->orderOfOperations++);//waiting for barber checks waiting room
+			fflush(outputFile);
 			sem_post(xvalek02_vypis);
 
-			sem_post(xvalek02_holic_dostrihal);//ceka nez ho holic dostriha
-
+			sem_post(xvalek02_holic_dostrihal); //waiting for barber cuts
 		}
 
 		return 0;
 	}
 
-	for ( i = 1 ; i <= p.pocet_zakazniku ; i++ )
-	{//tvorim zakazniky
-		usleep(rand() % ((p.genc*1000)+1)); //cas vytvoreni zakaznika
-		pid_t zakaznik = fork();
+	for ( i = 1 ; i <= p.numberOfCustomers ; i++ ) { //creating of customers
+		usleep(rand() % ((p.genc*1000)+1)); //customer's creating time
+		pid_t customer = fork();
 		
-		if ( zakaznik == -1 )
-		{//neporadilo se vytvorit proces
-			perror("fork() chyba");
+		//customer creating fail
+		if ( customer == -1 ) {
+			perror("fork() fail.");
 			break;
 		}
 
-		if ( zakaznik > 0 ) continue;
+		if ( customer > 0 )
+			continue;
 
 		sem_wait(xvalek02_vypis);
-		fprintf(vyst_soubor, "%u: customer %u: created\n", sdilena_data->poradi_operace++, i);
-		fflush(vyst_soubor);
+		fprintf(outputFile, "%u: customer %u: created\n", sharedData->orderOfOperations++, i);
+		fflush(outputFile);
 		sem_post(xvalek02_vypis);
 
-		if (sem_trywait(xvalek02_cekarna) == -1 && errno == EAGAIN)
-		{//nepodarilo se vstoupit do cekarny
+		//unable to enter waiting room
+		if (sem_trywait(xvalek02_cekarna) == -1 && errno == EAGAIN) {
 			sem_wait(xvalek02_vypis);
-			fprintf(vyst_soubor, "%u: customer %u: refused\n", sdilena_data->poradi_operace++, i);
-			fflush(vyst_soubor);
+			fprintf(outputFile, "%u: customer %u: refused\n", sharedData->orderOfOperations++, i);
+			fflush(outputFile);
 			sem_post(xvalek02_vypis);
 
-			//uklid
-			if (vyst_soubor != stdout) fclose(vyst_soubor);
+			//clean
+			if (outputFile != stdout)
+				fclose(outputFile);
+
 			sem_close(xvalek02_cekarna);
 			sem_close(xvalek02_vypis);
 			sem_close(xvalek02_holic_spi);
 			sem_close(xvalek02_holicova_zidle);
 			sem_close(xvalek02_holic_dostrihal);
 			sem_close(xvalek02_ready_vypisy);
-			shmdt((void*)sdilena_data);
+			shmdt((void*)sharedData);
 			exit(0);
 		}
 
-		//dostal jsem se do cekarny:
+		//customer is in waiting room
 		sem_wait(xvalek02_vypis);
-		fprintf(vyst_soubor, "%u: customer %u: enters\n", sdilena_data->poradi_operace++, i);
-		fflush(vyst_soubor);
+		fprintf(outputFile, "%u: customer %u: enters\n", sharedData->orderOfOperations++, i);
+		fflush(outputFile);
 		sem_post(xvalek02_vypis);
 
-		//zakaznik zbudi holice:
+		//customer wakes up barber:
 		sem_post(xvalek02_holic_spi);
 
-		//1 zakaznik se posadi:
+		//1 customer allocate chair :D:
 		sem_wait(xvalek02_holicova_zidle);
 		sem_post(xvalek02_cekarna);
 
 		sem_wait(xvalek02_ready_vypisy);
 
 		sem_wait(xvalek02_vypis);
-		fprintf(vyst_soubor, "%u: customer %u: ready\n", sdilena_data->poradi_operace++, i);
-		fflush(vyst_soubor);
+		fprintf(outputFile, "%u: customer %u: ready\n", sharedData->orderOfOperations++, i);
+		fflush(outputFile);
 		sem_post(xvalek02_vypis);
 
 		sem_wait(xvalek02_holic_dostrihal);
 
-		//uvolni holicovu zidli dalsimu zakaznikovi:
+		//release barber's chair to next customer:
 		sem_post(xvalek02_holicova_zidle);
 
 		sem_wait(xvalek02_vypis);
-		fprintf(vyst_soubor, "%u: customer %u: served\n", sdilena_data->poradi_operace++, i);
-		fflush(vyst_soubor);
+		fprintf(outputFile, "%u: customer %u: served\n", sharedData->orderOfOperations++, i);
+		fflush(outputFile);
 		sem_post(xvalek02_vypis);
 	
-		//uklid
-		if (vyst_soubor != stdout) fclose(vyst_soubor);
+		//clean
+		if (outputFile != stdout)
+			fclose(outputFile);
 		sem_close(xvalek02_cekarna);
 		sem_close(xvalek02_vypis);
 		sem_close(xvalek02_holic_spi);
 		sem_close(xvalek02_holicova_zidle);
 		sem_close(xvalek02_holic_dostrihal);
 		sem_close(xvalek02_ready_vypisy);
-		shmdt((void*)sdilena_data);
+		shmdt((void*)sharedData);
 		fflush(stdout);
 		exit(0);
 	}
 
-	for ( i-- ; i != 0 ; i--  )
-	{//ukoncujeme zakazniky
+	//customers ending of
+	for ( i-- ; i != 0 ; i--  )	{
 		wait(NULL);
 	}
 
 	kill(id, SIGUSR1);
 
 	waitpid(id, NULL, 0);
-	znic_semafory(xvalek02_cekarna, xvalek02_vypis, xvalek02_holic_spi, xvalek02_holicova_zidle, xvalek02_holic_dostrihal, xvalek02_ready_vypisy);
-	shmdt((void*)sdilena_data);
-	shmctl(pamet1,IPC_RMID, NULL);
-	if (vyst_soubor != stdout) fclose(vyst_soubor);
+	cleanSemaphores(xvalek02_cekarna, xvalek02_vypis, xvalek02_holic_spi, xvalek02_holicova_zidle, xvalek02_holic_dostrihal, xvalek02_ready_vypisy);
+	shmdt((void*)sharedData);
+	shmctl(sharedMemory,IPC_RMID, NULL);
+	if (outputFile != stdout)
+		fclose(outputFile);
 	return 0;
 }
